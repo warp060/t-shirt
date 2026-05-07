51beb1d1766dd871f36cb32fc1768ad5dc5ba680
Abbas Threads <abbas@threads.com>
2026-05-07 12:18:23 +0530

    Force IPv4 for SMTP to fix Render ENETUNREACH error

--- server/mailer.js
+++ server/mailer.js
@@ -11,6 +11,7 @@ const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
     secure: false, // Use STARTTLS
+    family: 4, // Force IPv4 to avoid ENETUNREACH errors on Render
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
